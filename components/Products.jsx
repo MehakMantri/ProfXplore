46ae3f2c9e25a427f7f3cb8863168aab5da3e354
shradha mantry <itsmehak27@gmail.com>
2024-08-21 02:55:42 +0530

    hero-section

---
+++ components/Products.jsx
@@ -0,0 +1,98 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+export const products = [
+    {
+      title: "Moonbeam",
+      link: "https://gomoonbeam.com",
+      thumbnail:"/images/img1.jpeg"
+
+    },
+    {
+      title: "Cursor",
+      link: "https://cursor.so",
+      thumbnail:
+        "/images/img2.jpeg",
+    },
+    {
+      title: "Rogue",
+      link: "https://userogue.com",
+      thumbnail:
+        "/images/img5.jpeg",
+    },
+   
+    {
+      title: "Editorially",
+      link: "https://editorially.org",
+      thumbnail:
+        "/images/img6.jpeg",
+    },
+    {
+      title: "Editrix AI",
+      link: "https://editrix.ai",
+      thumbnail:
+        "/images/img5.jpeg",
+    },
+    {
+      title: "Pixel Perfect",
+      link: "https://app.pixelperfect.quest",
+      thumbnail:
+        "/images/img3.jpeg",
+    },
+   
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      thumbnail:
+        "/images/img4.jpeg",
+    },
+    {
+      title: "Aceternity UI",
+      link: "https://ui.aceternity.com",
+      thumbnail:
+        "/images/img7.jpeg",
+    },
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      thumbnail:
+        "/images/img8.jpeg",
+    },
+    // {
+    //   title: "SmartBridge",
+    //   link: "https://smartbridgetech.com",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    // },
+    // {
+    //   title: "Renderwork Studio",
+    //   link: "https://renderwork.studio",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+    // },
+   
+    // {
+    //   title: "Creme Digital",
+    //   link: "https://cremedigital.com",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+    // },
+    // {
+    //   title: "Golden Bells Academy",
+    //   link: "https://goldenbellsacademy.com",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+    // },
+    // {
+    //   title: "Invoker Labs",
+    //   link: "https://invoker.lol",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+    // },
+    // {
+    //   title: "E Free Invoice",
+    //   link: "https://efreeinvoice.com",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+    // },
+  ];
